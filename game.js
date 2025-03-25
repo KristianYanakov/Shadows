4ae35da7081b4cd4a739b9057679b3c57465611e
KristianYanakov <christianyanakov@gmail.com>
2025-03-24 20:23:32 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,18 @@
+﻿
+function update() {
+    
+}
+
+function draw() {
+
+}
+
+function keyup(key) {
+    //console.log("Pressed", key);
+    
+}
+
+function mouseup() {
+    //console.log("Mouse clicked at", mouseX, mouseY);
+
+}
